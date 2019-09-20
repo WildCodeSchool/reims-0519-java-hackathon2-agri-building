3df11d52167a0fba7ef4925f9eb3aa3788a8193e
Rémy Trohel <remy.trohel@gmail.com>
2019-09-20 11:54:22 +0200

    added browse et read by expertise

--- src/main/java/fr/wildcodeschool/agriBuilding/repositories/AdherentRepository.java
+++ src/main/java/fr/wildcodeschool/agriBuilding/repositories/AdherentRepository.java
@@ -1,9 +1,14 @@
 package fr.wildcodeschool.agriBuilding.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import fr.wildcodeschool.agriBuilding.entities.Adherent;
 
 public interface AdherentRepository extends JpaRepository<Adherent, Long> {
     
+    @Query(value="SELECT * FROM adherent where first_expertise = ?1 OR second_expertise = ?1", nativeQuery = true)
+    public List<Adherent> findByExpertise(String expertise);
 }
